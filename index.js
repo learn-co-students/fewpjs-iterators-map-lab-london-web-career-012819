3bc8ff77131c4cfc3b73d6f0334155fac309e948
stubrew24 <sewelly@gmail.com>
2019-03-23 15:06:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const testVar = {}
+const tutorials = [
+  'what does the this keyword mean?', 
+  'What is the Contutorialuctor OO pattern?',
+  'implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?'
+];
 
-function testFunc() {
-  return "hi"
+function titlize(word){
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
+
+function titleCased(array){
+  return array.map(function(string){
+    let words = string.split(' ');
+    return words.map(titlize).join(' ');
+  });
+}
+
+ titleCased = titleCased(tutorials);
